7d763d836f55dfafc0602f506e32103d30caa574
Mridul <m.sarker@braincraftapps.com>
2023-03-06 13:08:09 +0600

    PHTDTRNDRD-3 Landing acitivty added.

--- app/src/main/java/com/example/photo_editor/HomeActivity.kt
+++ app/src/main/java/com/example/photo_editor/HomeActivity.kt
@@ -1,11 +1,25 @@
 package com.example.photo_editor
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.example.photo_editor.databinding.ActivityHomeBinding
+import com.example.photo_editor.editor.activity.EditorActivity
 
 class HomeActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityHomeBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.editor.setOnClickListener {
+           startActivity(Intent(this, EditorActivity::class.java))
+        }
+        binding.collage.setOnClickListener {
+            Toast.makeText(this, "Collage", Toast.LENGTH_SHORT).show()
+        }
     }
 }
