e8211cc6b1be7fec5ddc538fa7eb083c3e80e857
RaihanUddinPiash <raihan.uddin@braincraftapps.com>
2023-03-23 18:40:19 +0600

    PHTDTRNDRD-11 #comment Add individual scale gesture for collage template frames.

--- CollageEditor/src/main/java/com/bcl/android/collage_editor/customview/CustomView.kt
+++ CollageEditor/src/main/java/com/bcl/android/collage_editor/customview/CustomView.kt
@@ -38,19 +38,22 @@ class CustomView @JvmOverloads constructor(
     var corEffect = CornerPathEffect(0f)
     private var uriLists: ArrayList<Uri> = ArrayList()
     private var bitmapLists: ArrayList<Bitmap> = ArrayList()
-    private var mScaleDetector: ScaleGestureDetector? = null
-    private var mGestureDetector: GestureDetector? = null
-    private var mMatrix: Matrix? = null
-    private var oldRotation = 0f
-    private var newRotation = 0f
+    private var scaleDetectorList: ArrayList<ScaleGestureDetector> = ArrayList()
+    private var gestureDetectorList: ArrayList<GestureDetector> = ArrayList()
+    private var mMatrix: Matrix = Matrix()
+    private var matrixList: ArrayList<Matrix> = ArrayList()
     private lateinit var region: Region
+    private var regionList: ArrayList<Region> = ArrayList()
     private var pointerDown: Boolean = false
+    private var pointerRegionIndex: Int = -1
+    private var pathLists: ArrayList<Path> = ArrayList()
 
     init {
         path1 = Path()
         path2 = Path()
 
-        mMatrix = Matrix()
+        pathLists.add(path1)
+        pathLists.add(path2)
 
         paint1 = Paint(Paint.ANTI_ALIAS_FLAG or Paint.FILTER_BITMAP_FLAG)
         paint1.style = Paint.Style.FILL
@@ -81,25 +84,30 @@ class CustomView @JvmOverloads constructor(
 
         })
 
-        mScaleDetector = ScaleGestureDetector(context, object : SimpleOnScaleGestureListener() {
-            override fun onScale(detector: ScaleGestureDetector): Boolean {
-                val factor = detector.scaleFactor
-                mMatrix!!.postScale(factor, factor, width / 2f, height / 2f)
-                ViewCompat.postInvalidateOnAnimation(this@CustomView)
-                return true
-            }
-        })
-
-        mGestureDetector =
-            GestureDetector(context, object : GestureDetector.SimpleOnGestureListener() {
-                override fun onScroll(
-                    e1: MotionEvent, e2: MotionEvent, distanceX: Float, distanceY: Float
-                ): Boolean {
-                    mMatrix!!.postTranslate(-distanceX, -distanceY)
+        for (i in pathLists.indices) {
+            val sDetector = ScaleGestureDetector(context, object : SimpleOnScaleGestureListener() {
+                override fun onScale(detector: ScaleGestureDetector): Boolean {
+                    val factor = detector.scaleFactor
+                    matrixList[i].postScale(factor, factor, width / 2f, height / 2f)
                     ViewCompat.postInvalidateOnAnimation(this@CustomView)
                     return true
                 }
             })
+
+            val gDetector =
+                GestureDetector(context, object : GestureDetector.SimpleOnGestureListener() {
+                    override fun onScroll(
+                        e1: MotionEvent, e2: MotionEvent, distanceX: Float, distanceY: Float
+                    ): Boolean {
+                        matrixList[i].postTranslate(-distanceX, -distanceY)
+                        ViewCompat.postInvalidateOnAnimation(this@CustomView)
+                        return true
+                    }
+                })
+
+            scaleDetectorList.add(sDetector)
+            gestureDetectorList.add(gDetector)
+        }
     }
 
     fun updateZoomScale(scale: Float) {
@@ -150,25 +158,29 @@ class CustomView @JvmOverloads constructor(
         path1.lineTo((0f + gapBetweenFrames) * w, (1f - gapBetweenFrames) * h)
         path1.close()
 
-        val rectF = RectF()
-        path1.computeBounds(rectF, true)
-        region = Region()
-        region.setPath(
-            path1, Region(
-                rectF.left.toInt(), rectF.top.toInt(), rectF.right.toInt(), rectF.bottom.toInt()
-            )
-        )
-
         path2.moveTo((0.3f + gapBetweenFrames) * w, (0f + gapBetweenFrames) * h)
         path2.lineTo((1f - gapBetweenFrames) * w, (0f + gapBetweenFrames) * h)
         path2.lineTo((1f - gapBetweenFrames) * w, (1f - gapBetweenFrames) * h)
         path2.lineTo((0.70f + gapBetweenFrames) * w, (1f - gapBetweenFrames) * h)
         path2.close()
 
+        for (i in pathLists.indices) {
+            val rectF = RectF()
+            pathLists[i].computeBounds(rectF, true)
+            region = Region()
+            region.setPath(
+                pathLists[i], Region(
+                    rectF.left.toInt(), rectF.top.toInt(), rectF.right.toInt(), rectF.bottom.toInt()
+                )
+            )
+            regionList.add(region)
 
-        val scale: Float = max(w / (width / 2f), h / (height / 2f))
-        mMatrix!!.setScale(scale, scale)
-        mMatrix!!.postTranslate((w - scale * (width / 2f)) / 2f, (h - scale * (height / 2f)) / 2f)
+            val scale: Float = max(w / (width / 2f), h / (height / 2f))
+            mMatrix = Matrix()
+            mMatrix.setScale(scale, scale)
+            mMatrix.postTranslate((w - scale * (width / 2f)) / 2f, (h - scale * (height / 2f)) / 2f)
+            matrixList.add(mMatrix)
+        }
     }
 
     @RequiresApi(Build.VERSION_CODES.O)
@@ -180,18 +192,17 @@ class CustomView @JvmOverloads constructor(
         paint1.pathEffect = corEffect
         paint2.pathEffect = corEffect
 
-        // canvas?.drawPath(path1, paint1)
-        //  canvas?.drawPath(path2, paint2)
-
         canvas?.save()
-        canvas?.clipPath(path1)
-        canvas?.drawBitmap(bitmapLists[0], mMatrix!!, paint1)
+        for (i in pathLists.indices) {
+            canvas?.clipPath(pathLists[i])
+            canvas?.drawBitmap(bitmapLists[i], matrixList[i], paint1)
+            canvas?.restore()
+            canvas?.save()
+        }
 
-        canvas?.restore()
-        canvas?.save()
 
-        canvas?.clipPath(path2)
-        canvas?.drawBitmap(bitmapLists[1], Matrix(), paint2)
+//        canvas?.clipPath(path2)
+//        canvas?.drawBitmap(bitmapLists[1], Matrix(), paint2)
 
         canvas?.restore()
     }
@@ -202,15 +213,19 @@ class CustomView @JvmOverloads constructor(
 
         val point = Point()
         point.x = event!!.x.toInt()
-        point.y = event!!.y.toInt()
+        point.y = event.y.toInt()
 
-        when (event!!.action and MotionEvent.ACTION_MASK) {
+        when (event.action and MotionEvent.ACTION_MASK) {
             MotionEvent.ACTION_DOWN -> {
-                if (region.contains(point.x, point.y)) pointerDown = true
+                for (i in regionList.indices) {
+                    if (regionList[i].contains(point.x, point.y)) {
+                        pointerDown = true
+                        pointerRegionIndex = i
+                    }
+                }
             }
 
             MotionEvent.ACTION_MOVE -> {
-//                pointerDown = true
             }
 
             MotionEvent.ACTION_UP -> {
@@ -218,9 +233,9 @@ class CustomView @JvmOverloads constructor(
             }
         }
 
-        if (pointerDown || region.contains(point.x, point.y)) {
-            event?.let { mGestureDetector!!.onTouchEvent(it) }
-            event?.let { mScaleDetector!!.onTouchEvent(it) }
+        if (pointerDown || regionList[pointerRegionIndex].contains(point.x, point.y)) {
+            event.let { gestureDetectorList[pointerRegionIndex].onTouchEvent(it) }
+            event.let { scaleDetectorList[pointerRegionIndex].onTouchEvent(it) }
         }
 
         return true
